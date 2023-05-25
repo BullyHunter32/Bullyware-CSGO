9382ff4e8b2bb7fc9ae74ca419a9a04be41cfa3b
BullyHunter32 <60613196+BullyHunter32@users.noreply.github.com>
2023-05-25 18:15:07 +0100

    matrix3x4_t::GetVector()

--- Bullyware-CSGO/Bullyware/SDK/matrix3x4.h
+++ Bullyware-CSGO/Bullyware/SDK/matrix3x4.h
@@ -1,10 +1,16 @@
 #pragma once
+#include "Vector.h"
 
 struct matrix3x4_t
 {
 	float* operator[](int i)
 	{
 		return m_matrix[i];
+	}
+
+	Vector GetVector(int i)
+	{
+		return Vector(this[i][0][3], this[i][1][3], this[i][2][3]);
 	}
 
 	float m_matrix[3][4];
